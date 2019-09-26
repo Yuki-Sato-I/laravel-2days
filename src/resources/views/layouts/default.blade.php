b67933f78cdce86959adf69cee0b5e788226a8bc
Yuki-Sato-I <yukiyukiamam@gmail.com>
2019-09-26 11:46:05 +0900

    home index css調整

--- src/resources/views/layouts/default.blade.php
+++ src/resources/views/layouts/default.blade.php
@@ -8,6 +8,7 @@
   </title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="{{ asset('css/layouts/header.css') }}">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
   @yield('css')
   @yield('script')
 </head>
