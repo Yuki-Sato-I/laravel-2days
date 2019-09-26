946ebfa8fffe295ad53219af7855f6fcf4f6e38c
Yuki-Sato-I <yukiyukiamam@gmail.com>
2019-09-26 12:32:51 +0900

    user show css調整

--- src/resources/views/user/show.blade.php
+++ src/resources/views/user/show.blade.php
@@ -5,7 +5,7 @@
 @endphp
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/like/index.css') }}">
+<link rel="stylesheet" href="{{ asset('css/user/show.css') }}">
 @endsection
 
 {{-- @section('script')
@@ -15,14 +15,30 @@
 @section('title',$user->github_id.'のページ')
 
 @section('content')
-    @if (!$user->image)
-        <img src="{{ asset('image/noimage.png') }}" width="100" height="100">
-    @else 
-        <img src="{{ $user->image }}" width="100" height="100">
-    @endif
-    {{ $user->github_id }}
-    いいねカウント数{{ count($user->likes) }}
+    <div class="container">
+        <div class="user-container">
+            <div class="user-div">
+                @if (!$user->image)
+                    <img src="{{ asset('image/noimage.png') }}" width="250" height="250">
+                @else 
+                    <img src="{{ $user->image }}" width="100" height="100">
+                @endif
+            </div>
+            <div class="user-div">
+                <div class="user-font">{{ $user->github_id }}</div>
+            </div>
+            <div class="good">
+                <div class="container">
+                    <span class="user-font">いいねカウント数</span>
+                    <br>
+                    <span>{{ count($user->likes) }}</span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
     @foreach ($posts as $post)
-        <img src="{{ $post->image }}" width="100" height="100"></a>
+        <img src="{{ $post->image }}" width="300" height="300"></a>
     @endforeach
 @endsection
